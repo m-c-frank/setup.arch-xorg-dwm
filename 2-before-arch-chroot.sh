884bf4a70c00362d9dd30d0d04e290104ccdb90f
Martin Christoph Frank <martinchristophfrank@gmail.com>
2023-11-12 00:36:31 +0100

    more interactive before

--- 2-before-arch-chroot.sh
+++ 2-before-arch-chroot.sh
@@ -1,49 +1,96 @@
 #!/bin/bash
 
-# Arch Linux Installation - Part 1
+# Welcome Message
+echo "Welcome to the Arch Linux Installation Helper!"
+echo "This script will guide you through identifying, partitioning, formatting, and mounting your drive."
 
-# Update the system clock
-timedatectl set-ntp true
-echo "System clock updated."
-
-# List available disks
-echo "Available disks:"
+# List all drives
+echo "Listing all drives:"
 lsblk
 
-# Ask for the root partition
-read -p "Enter the root partition (e.g., /dev/sda2): " root_partition
-while [[ ! -b "$root_partition" ]]; do
-    echo "Partition does not exist. Please try again."
-    read -p "Enter the root partition (e.g., /dev/sda2): " root_partition
-done
+# Checking for EFI or BIOS system
+if [ -d /sys/firmware/efi/efivars ]; then
+    echo "EFI system detected."
+    is_efi=true
+else
+    echo "BIOS system detected."
+    is_efi=false
+fi
 
-# Ask for the EFI partition
-read -p "Enter the EFI partition (e.g., /dev/sda1): " efi_partition
-while [[ ! -b "$efi_partition" ]]; do
-    echo "Partition does not exist. Please try again."
-    read -p "Enter the EFI partition (e.g., /dev/sda1): " efi_partition
-done
+# Ask user to identify the target drive
+read -p "Please enter the device name of the target drive (e.g., sda, sdb): " target_drive
+echo "You have selected /dev/$target_drive"
 
-# Mount the partitions
-echo "Mounting $root_partition to /mnt..."
-mount "$root_partition" /mnt
-mkdir -p /mnt/boot
-echo "Mounting $efi_partition to /mnt/boot..."
-mount "$efi_partition" /mnt/boot
+# Warning message
+echo "WARNING: All data on /dev/$target_drive will be lost! Proceed with caution."
 
-# Install essential packages
-echo "Installing essential packages..."
+# Confirmation
+read -p "Are you sure you want to continue? (yes/no): " confirmation
+if [ "$confirmation" != "yes" ]; then
+    echo "Operation cancelled."
+    exit 1
+fi
+
+# Partitioning Instructions
+if $is_efi; then
+    echo "You will now enter 'fdisk' to partition your drive for an EFI system."
+    echo "Follow these steps inside fdisk:"
+    echo "  1. Type 'g' to create a new empty GPT partition table."
+    echo "  2. Type 'n' to add a new partition. For the EFI system partition, select defaults and set size to +550M."
+    echo "  3. Type 'n' again to add your root partition. Select defaults and assign the remaining space or the size you prefer."
+    echo "  4. Type 't' to change the partition type. Select partition 1 and set type to 'EFI System'."
+    echo "  5. Type 'w' to write the changes and exit fdisk."
+else
+    echo "You will now enter 'fdisk' to partition your drive for a BIOS system."
+    echo "Follow these steps inside fdisk:"
+    echo "  1. Type 'o' to create a new empty DOS partition table."
+    echo "  2. Type 'n' to add a new partition. Select defaults for a primary partition and assign the size you prefer."
+    echo "  3. Type 'w' to write the changes and exit fdisk."
+fi
+
+# Start fdisk
+echo "Starting fdisk for /dev/$target_drive"
+fdisk /dev/$target_drive
+
+# Formatting the partitions
+echo "Formatting the partitions..."
+if $is_efi; then
+    echo "Formatting the EFI system partition as FAT32..."
+    mkfs.fat -F32 /dev/${target_drive}1
+    echo "Formatting the root partition as ext4..."
+    mkfs.ext4 /dev/${target_drive}2
+    root_partition="/dev/${target_drive}2"
+    efi_partition="/dev/${target_drive}1"
+else
+    echo "Formatting the root partition as ext4..."
+    mkfs.ext4 /dev/${target_drive}1
+    root_partition="/dev/${target_drive}1"
+fi
+
+# Mount the root partition
+echo "Mounting the root partition..."
+mount $root_partition /mnt
+
+# Mount the EFI partition (if EFI system)
+if $is_efi; then
+    echo "Mounting the EFI system partition..."
+    mkdir -p /mnt/boot/EFI
+    mount $efi_partition /mnt/boot/EFI
+fi
+
+# Base system installation
+echo "Installing the base system..."
 pacstrap /mnt base linux linux-firmware
 
-# Generate fstab
+# Generating fstab
 echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
-echo "fstab generated."
+cat /mnt/etc/fstab
 
-echo "Base installation complete. Please run the second script after arch-chroot."
+# Ready to chroot
+echo "You are now ready to chroot into your new system."
+echo "Type 'arch-chroot /mnt' to switch into the new system environment."
+echo "Installation Helper Complete!"
 
-curl -LO https://raw.githubusercontent.com/mcfrank/setup.arch-xorg-dwm/main/2-after-arch-chroot
+# End of the script
 
-mv 2-after-arch-chroot.sh /mnt/minimalinstall
-
-echo "run arch-chroot /mnt
